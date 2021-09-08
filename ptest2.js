b7dec64f0abee6fe792c3b61375a4baa70cddaae
MD. Sunjidul Islam <43532784+Niloy33@users.noreply.github.com>
2021-09-08 20:36:30 +0600

    Add files via upload

---
+++ ptest2.js
@@ -0,0 +1,15 @@
+var x, y, z;
+x = window.prompt("enter the num:");
+y = window.prompt("enter the num:");
+z = window.prompt("enter the num:");
+if (x > 0 && y > 0 && z > 0) {
+  alert(" the sign is +");
+} else if (x < 0 && y < 0 && z < o) {
+  console.log(" the sign is -");
+} else if (x > 0 && y < 0 && z < 0) {
+  console.log("the sign is +");
+} else if (x < 0 && y > 0 && z < 0) {
+  console.log("The sign is +");
+} else {
+  alert("The sign is -");
+}
