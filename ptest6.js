9d4bd97117be33ebdaa2a9d4a60cf7ab5d583c90
MD. Sunjidul Islam <43532784+Niloy33@users.noreply.github.com>
2021-09-15 21:28:20 +0600

    Add files via upload
    there is some error in this

---
+++ ptest6.js
@@ -0,0 +1,12 @@
+console.log("hello");
+
+function multiplyBy() {
+  num1 = document.getElementById("firstNumber").value;
+  num2 = document.getElementById("second number").value;
+  document.getElementById("result").innerHTML = num1 * num2;
+}
+function divideBy() {
+  num1 = document.getElementById("firstNumber").value;
+  num2 = document.getElementById("second number").value;
+  document.getElementById("result").innerHTML = num1 / num2;
+}
